7a582434527c1a1d04604ab93b17aebe9cf3dec3
Ekaterina Rykova <katrykova2002@yandex.ru>
2023-09-10 10:09:33 +0300

    Module

--- app/src/main/java/com/example/dagger_hilt_lesson/WiFiManaget.kt
+++ app/src/main/java/com/example/dagger_hilt_lesson/WiFiManaget.kt
@@ -5,20 +5,21 @@ import dagger.hilt.android.scopes.ActivityScoped
 import javax.inject.Inject
 import javax.inject.Singleton
 
-@ActivityScoped
-class WiFiManager @Inject constructor(private val settings: WiFiSettings) {
+class WiFiManager(private val settings: WiFiSettings) {
     fun connect() {
         settings.openConnection()
     }
+
     fun sendMessage() {
         settings.writeBytes()
     }
 }
 
-class WiFiSettings @Inject constructor() {
+class WiFiSettings {
     fun openConnection() {
         Log.d("MyLog", "Connected")
     }
+
     fun writeBytes() {
         Log.d("MyLog", "Hello!")
     }
